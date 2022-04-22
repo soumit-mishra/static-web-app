6d8f8750bc0391b407c744a30054e7c4b9afae9b
Soumit Mishra <soumitkumar.mishra@knauf.com>
2022-04-22 16:32:31 +0200

    add the maintenace page

---
+++ spa/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      <div id="bottom">
+      <p>
+        SITE UNDER MAINTENANCE
+      </p>
+      </div>
+    
+      </header>
+  </div>
+  );
+}
+
+export default App;
